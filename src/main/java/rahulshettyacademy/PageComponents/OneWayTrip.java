ed9f3880e827fc7108e5d1548c97ad9e01cec4f7
yadvendra-git <yadverma@gmail.com>
2022-02-04 09:17:16 +0530

    Second commit for dp

--- src/main/java/rahulshettyacademy/PageComponents/OneWayTrip.java
+++ src/main/java/rahulshettyacademy/PageComponents/OneWayTrip.java
@@ -9,5 +9,8 @@ public class OneWayTrip implements SearchFlightAvailability {
     @Override
     public void checkAvailability(HashMap<String, String> reservationDetails) {
         System.out.println("In checkAvailability method of oneWayTrip class");
+        System.out.println("change in the code");
     }
+
+
 }
